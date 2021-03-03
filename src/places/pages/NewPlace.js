a68c0f31b8d80b0ec279463d0c149e10485a4998
jpratik15 <190010034@iitdh.ac.in>
2021-03-03 12:47:18 +0530

    Add Setup,Pages,Routes and Folder structure

---
+++ src/places/pages/NewPlace.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const NewPlace = () => {
+    return <h1>NewPlace Works!!</h1>
+}
+export default NewPlace;
---
+++ src/users/pages/Users.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const Users = () => {
+    return <h1>User Works!!</h1>
+}
+export default Users;
