69a3f3564cc28271f95cd9414519e4650d3b0677
jpratik15 <190010034@iitdh.ac.in>
2021-03-03 15:37:16 +0530

    Add card,avatar component and styling

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -4,12 +4,12 @@ import UsersList from "../components/UsersList"
 
 const Users = () => {
     const USERS = [
-        // {
-        //     id : "u1",
-        //     name : "Pratik",
-        //     image: "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80",
-        //     places : 1
-        // }
+        {
+            id : "u1",
+            name : "Pratik",
+            image: "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80",
+            places : 1
+        }
     ]
     return <UsersList items={USERS} />
 }
