91879c31cced2c42f7c83d6fd95f08e5fcde4a20
jpratik15 <190010034@iitdh.ac.in>
2021-03-04 21:16:01 +0530

    Add backdrop portal for closing side drawer and drawer state

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,22 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Link } from "react-router-dom";
 import MainHeader from "./MainHeader";
 import NavLink from "./NavLinks";
 import SideDrawer from "./SideDrawer";
+import BackDrop from "../../components/UIElements/BackDrop"
 
 import "./MainNavigation.css";
 
 const MainNavigation = (props) => {
+  let [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawer = () => {
+    setDrawerIsOpen(true);
+  }
+
+  const closeDrawer = () => {
+    setDrawerIsOpen(false); 
+  }
+
   return (
     <React.Fragment>
-      <SideDrawer>
-        <nav className="main-navigation__drawer-nav">
-          <NavLink />
-        </nav>
-      </SideDrawer>
+      
+      {drawerIsOpen ? <BackDrop onClick={closeDrawer}/> :null }
+
+      {drawerIsOpen ? (
+        <SideDrawer>
+          <nav className="main-navigation__drawer-nav">
+            <NavLink />
+          </nav>
+        </SideDrawer>
+      ) : null}
       <MainHeader>
-        <button className="main-navigation__menu-btn">
+        <button className="main-navigation__menu-btn" onClick={openDrawer}>
           <span />
           <span />
           <span />
