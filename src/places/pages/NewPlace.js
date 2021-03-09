5b548a1d8b7b760f930858ee98a952efe2e4def2
jpratik15 <190010034@iitdh.ac.in>
2021-03-09 10:44:31 +0530

    Add input validator component and logic

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 import Input from  "../../shared/components/FormElements/Input"
-
+import {VALIDATOR_REQUIRE} from "../../shared/util/validators"
 import "./NewPlace.css"
 
 const NewPlace = () => {
     return(
         <form className="place-form">
-            <Input element="input" type="text" label="title" validator={[]} errorText="Please Enter A Valid Title"/>
+            <Input element="input" type="text" label="title" validators={[VALIDATOR_REQUIRE()]} errorText="Please Enter A Valid Title"/>
         </form>
     )
 }
